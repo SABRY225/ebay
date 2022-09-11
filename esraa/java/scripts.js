f9c31d6e59971f445327f533e4c4409cc4d24a07
esraaelgehany <esraa.abdallrhem40@gmail.com>
2022-09-11 02:53:26 +0200

    added check out page

---
+++ esraa/java/scripts.js
@@ -0,0 +1,30 @@
+
+var inp2=document.getElementById("").value;
+var inp3=document.getElementById("").value;
+var inp4=document.getElementById("").value;
+var inp5=document.getElementById("").value;
+
+var p1=document.getElementById("par1");
+p1=document.write(inp.value);
+var p2=document.getElementById("par2").innerHTML(inp2);
+var p3=document.getElementById("par3").innerHTML(inp3);
+var p4=document.getElementById("par4").innerHTML(inp4);
+var p5=document.getElementById("par5").innerHTML(inp5);
+
+function text()
+{
+     
+    document.getElementById("par1").innerHTML=document.getElementById("inp1").value;
+    document.getElementById("par2").innerHTML=document.getElementById("inp2").value;
+    document.getElementById("par3").innerHTML=document.getElementById("inp3").value;
+    document.getElementById("par4").innerHTML=document.getElementById("inp4").value;
+    document.getElementById("par5").innerHTML=document.getElementById("inp5").value;
+}
+// function choosenoption()
+// {
+    
+
+//     if (document.getElementById("radio111").is(':checked')) {
+//       document.getElementById("btn10").innerHTML="visa";
+//     }    
+// }
